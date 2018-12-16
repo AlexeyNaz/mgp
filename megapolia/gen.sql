b3c98ce34d398d161b3ac73a4b3cf5faabc2d8f9
anaza <anazarov94@gmail.com>
2018-12-16 22:14:11 +0700

    admin

--- megapolia/gen.sql
+++ megapolia/gen.sql
@@ -7,26 +7,26 @@ VALUES (20, '' golf '', '' Мини гольф '', '''', 6, 2, '' wlt7pk '');
 INSERT INTO db_mgp.megagames_activity (id, code, `desc`, comment, win, play, password)
 VALUES (21, '' velo '', '' Велооркестр '', '''', 4, 2, '' 2tp9s6 '');
 INSERT INTO db_mgp.megagames_activity (id, code, `desc`, comment, win, play, password)
-VALUES (22, '' mobracing '', '' Командная игра «Гонки на мобильных телефонах»'', '''', 2, 1, '' oadw77 '');
+VALUES (22, '' mobracing '', '' Командная игра «Гонки на мобильных телефонах»'', '', 2, 1, '' oadw77 '');
 INSERT INTO db_mgp.megagames_activity (id, code, `desc`, comment, win, play, password)
-VALUES (23, '' world '', '' Интерактивная зона «Оживающий мир» '', '''', 2, 2, '' yu7x7h '');
+VALUES (23, '' world '', '' Интерактивная зона «Оживающий мир» '', '', 2, 2, '' yu7x7h '');
 INSERT INTO db_mgp.megagames_activity (id, code, `desc`, comment, win, play, password)
-VALUES (24, '' game '', '' Game- zone '', '''', 2, 1, '' ovz8r2 '');
+VALUES (24, '' game '', '' Game- zone '', '', 2, 1, '' ovz8r2 '');
 INSERT INTO db_mgp.megagames_activity (id, code, `desc`, comment, win, play, password)
-VALUES (25, '' megacheckers '', '' Горячительные Мегашашки '', '''', 2, 1, '' br2v04 '');
+VALUES (25, '' megacheckers '', '' Горячительные Мегашашки '', '', 2, 1, '' br2v04 '');
 INSERT INTO db_mgp.megagames_activity (id, code, `desc`, comment, win, play, password)
-VALUES (26, '' poligon '', '' Мастер-класс по полигональным маскам компьютерных персонажей '', '''', 6, 6,
+VALUES (26, '' poligon '', '' Мастер-класс по полигональным маскам компьютерных персонажей '', '', 6, 6,
         '' 9sdtmp '');
 INSERT INTO db_mgp.megagames_activity (id, code, `desc`, comment, win, play, password)
-VALUES (27, '' bar '', '' Мастер-класс по приготовлению коктейлей '', '''', 1, 1, '' kr9jz7 '');
+VALUES (27, '' bar '', '' Мастер-класс по приготовлению коктейлей '', '', 1, 1, '' kr9jz7 '');
 INSERT INTO db_mgp.megagames_activity (id, code, `desc`, comment, win, play, password)
-VALUES (28, '' fly '', '' Мастер-класс по флейрингу '', '''', 2, 2, '' 1mbtl0 '');
+VALUES (28, '' fly '', '' Мастер-класс по флейрингу '', '', 2, 2, '' 1mbtl0 '');
 INSERT INTO db_mgp.megagames_activity (id, code, `desc`, comment, win, play, password)
-VALUES (29, '' freeze '', '' Крио-бар '', '''', 1, 1, '' tkiciq '');
+VALUES (29, '' freeze '', '' Крио-бар '', '', 1, 1, '' tkiciq '');
 INSERT INTO db_mgp.megagames_activity (id, code, `desc`, comment, win, play, password)
-VALUES (30, '' spin '', '' Видео Спиннер с оформлением в стилистике Мегафон '', '''', 0, 0, '' of3wzq '');
+VALUES (30, '' spin '', '' Видео Спиннер с оформлением в стилистике Мегафон '', '', 0, 0, '' of3wzq '');
 INSERT INTO db_mgp.megagames_activity (id, code, `desc`, comment, win, play, password)
-VALUES (31, '' master '', '' Мастер игры '', '''', 2, 1, '' sr676m '');
+VALUES (31, '' master '', '' Мастер игры '', '', 2, 1, '' sr676m '');
 INSERT INTO db_mgp.megagames_activity (id, code, `desc`, comment, win, play, password)
 VALUES (32, '' vr '', '' VR очки '', '''', 1, 1, '' ymk0ao '');
 INSERT INTO db_mgp.megagames_activity (id, code, `desc`, comment, win, play, password)
